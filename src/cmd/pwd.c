751aa79d69154550c2b7173f0161f31e00d31490
Adrien <adrien.pichard@epitech.eu>
2020-04-17 11:22:41 +0200

    FIX: status code on pwd

--- src/cmd/pwd.c
+++ src/cmd/pwd.c
@@ -15,7 +15,7 @@ void pwd(client_t *client, char *data UNUSED)
     char *res = calloc(strlen(client->wd) + 18, sizeof(char));
 
     raise_error(res != NULL, "calloc() ");
-    strcat(res, "227 \"");
+    strcat(res, "257 \"");
     strcat(res, client->wd);
     strcat(res, "\" created.\r\n");
     write_q(client, res, false);
